44cdad259d6c6b0b633f8dbe6cd07853471f33a1
Lisa Bergström <lisa.bergstrom@live.se>
2022-06-03 15:08:37 +0200

    added openlayer map

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,38 +1,61 @@
 /* eslint-disable */
-import React, { useState } from "react";
 
-const FindMyLocation = () => {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [status, setStatus] = useState(null);
+// import Map from "ol/Map";
+// import OSM from "ol/source/OSM";
+// import View from "ol/View";
+// import React, { useState } from "react";
 
-  const getLocation = () => {
-    if (!navigator.geolocation) {
-      setStatus("Geolocation is not supported by your browser");
-    } else {
-      setStatus("Locating...");
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setStatus(null);
-          setLat(position.coords.latitude);
-          setLng(position.coords.longitude);
-        },
-        () => {
-          setStatus("Unable to retrieve your location");
-        }
-      );
-    }
-  };
+// const FindMyLocation = () => {
+//   const [lat, setLat] = useState(null);
+//   const [lng, setLng] = useState(null);
+//   const [status, setStatus] = useState(null);
 
-  return (
-    <div className="App">
-      <button onClick={getLocation}>Get Location</button>
-      <h1>Coordinates</h1>
-      <p>{status}</p>
-      {lat && <p>Latitude: {lat}</p>}
-      {lng && <p>Longitude: {lng}</p>}
-    </div>
-  );
-};
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
 
-export default FindMyLocation;
+const Location = new Map({
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 0,
+  }),
+
+  //   const getLocation = () => {
+  //     if (!navigator.geolocation) {
+  //       setStatus("Geolocation is not supported by your browser");
+  //     } else {
+  //       setStatus("Locating...");
+  //       navigator.geolocation.getCurrentPosition(
+  //         (position) => {
+  //           setStatus(null);
+  //           setLat(position.coords.latitude);
+  //           setLng(position.coords.longitude);
+  //         },
+  //         () => {
+  //           setStatus("Unable to retrieve your location");
+  //         }
+  //       );
+  //     }
+  //   };
+
+  //   return (
+  //     <div className="App">
+  //       <button onClick={getLocation}>Where on Earth am I?</button>
+  //       <h1>Coordinates</h1>
+  //       <p>{status}</p>
+  //       {lat && <p>Latitude: {lat}</p>}
+  //       {lng && <p>Longitude: {lng}</p>}
+  //     </div>
+  //   );
+  // };
+});
+export default Location;
+
+// Heading: Where on Earth am I?
