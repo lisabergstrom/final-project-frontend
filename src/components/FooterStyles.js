ec237ce8f19f1dd850bfb4e89c07411c3563b84b
Lisa Bergström <lisa.bergstrom@live.se>
2022-06-15 16:02:17 +0200

    added list

--- src/components/FooterStyles.js
+++ src/components/FooterStyles.js
@@ -5,12 +5,15 @@ export const Box = styled.div`
 background: #212529;
 margin-bottom: 0;
 width: 100%;
+
+
 `;
 
 export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+	
     	
 
 `
